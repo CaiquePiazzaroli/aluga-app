db5dc378c2b3c16351566fbaf99492480994a4e9
caique <caique405@gmail.com>
2026-06-22 18:11:47 -0300

    Entendendo as operações de CRUD usando Hibernate

--- src/main/java/AlugaApp.java
+++ src/main/java/AlugaApp.java
@@ -1,6 +1,7 @@
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import model.Locatario;
 
 public class AlugaApp {
 
@@ -8,12 +9,49 @@ public class AlugaApp {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("aluga.app");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        try {
-            System.out.println("--- Inicializando o Hibernate e criando tabelas... ---");
-        } finally {
-            entityManager.close();
-            entityManagerFactory.close();
-            System.out.println("--- Recursos fechados com sucesso! ---");
-        }
+
+        // Salvando um Locatario (CREATE)
+//        Locatario locatario1 = new Locatario();
+//        // locatario1.setId(7985L); -> Sem ID
+//        locatario1.setNome("Jose Roberto Gomes");
+//
+//        entityManager.getTransaction().begin();
+//        entityManager.persist(locatario1);
+//        entityManager.getTransaction().commit();
+
+
+        // Salvando um Locatario (CREATE) mas usando Merge
+//        Locatario locatarioCriarComMerge = new Locatario();
+//        locatarioCriarComMerge.setNome("Matematico Pereira Portugues");
+//
+//        entityManager.getTransaction().begin();
+//        entityManager.merge(locatarioCriarComMerge);
+//        entityManager.getTransaction().commit();
+
+            // Atualizando um Locatario (UPDATE)
+//            Locatario locatarioAtualizar = new Locatario();
+//            locatarioAtualizar.setNome("Jose Almeida");
+//            locatarioAtualizar.setId(1L); // OBRIGATÓRIO PARA UPDATE
+//
+//            entityManager.getTransaction().begin();
+//            entityManager.merge(locatarioAtualizar);
+//            entityManager.getTransaction().commit();
+
+
+          // Buscando um Locatario por id (READ)
+//            Locatario l1 = entityManager.find(Locatario.class, 1);
+//            System.out.println(l1.getId());
+//            System.out.println(l1.getNome());
+
+        // Deletando um objeto (DELETE)
+        Locatario objetoExistente = entityManager.find(Locatario.class, 2);
+        entityManager.getTransaction().begin();
+        entityManager.remove(objetoExistente);
+        entityManager.getTransaction().commit();
+
+
+        entityManager.close();
+        entityManagerFactory.close();
+
     }
 }
--- src/main/java/model/Locatario.java
+++ src/main/java/model/Locatario.java
@@ -5,10 +5,34 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "Locatarios")
 public class Locatario {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "nome_locatario")
     private String nome;
+
+
+    public Locatario() {}
+
+    public Locatario(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
